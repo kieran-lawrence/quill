f47f5291c31b5347f7e4b985717621ab6314db0e
Kieran Lawrence <kieran.lawrence@wanews.com.au>
2024-10-08 09:42:27 +0800

    setup blank project with nx

---
+++ apps/quill-frontend/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'quill-frontend',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/quill-frontend',
+};
