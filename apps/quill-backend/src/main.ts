44c35e7691859681fa69a4772bc3dcf8c3abebbe
Kieran Lawrence <kieran.lawrence@wanews.com.au>
2024-11-01 17:38:59 +0800

    Housekeeping

--- apps/quill-backend/src/main.ts
+++ apps/quill-backend/src/main.ts
@@ -1,22 +1,24 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
+import { Logger } from '@nestjs/common'
+import { NestFactory } from '@nestjs/core'
 
-import { Logger } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
-
-import { AppModule } from './app/app.module';
+import { AppModule } from './app/app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
-}
+    const { PORT } = process.env
 
-bootstrap();
+    const app = await NestFactory.create(AppModule)
+    const globalPrefix = 'api'
+    app.setGlobalPrefix(globalPrefix)
+    app.enableCors({ origin: [`http://localhost:3000`], credentials: true })
+
+    try {
+        await app.listen(PORT, () =>
+            Logger.log(
+                `🚀 Application is running on: http://localhost:${PORT}/${globalPrefix}`,
+            ),
+        )
+    } catch (err) {
+        Logger.error(`🔥 Oh noez, something went wrong! 🔥`, err)
+    }
+}
+bootstrap()
